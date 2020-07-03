f7c0affbca612d59b08e7610b16f325523efce28
Mihajlo Maksa <mihajlomaksa9@gmail.com>
2020-07-03 13:30:31 +0200

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# sfdbgd
+# Node.js with PayPal REST SDK
 
-gbsdobeaoef[o]
+A simple example showing how to use the PayPal REST SDK with Node.js to take a payment from your website's visitors.
+
+## Quick Start
+
+Install Dependencies
+
+`npm install`
+
+Serve in the Browser
+
+`npm start`
---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const ejs = require('ejs');
+const paypal = require('paypal-rest-sdk');
+const port = process.env.PORT || 3000;
+
+paypal.configure({
+  mode: 'sandbox', // sandbox or live
+  client_id:
+    'AVPShnkLjAwnWjiswxTY-ljCL3ULMaKX3y8zBbTakl6maoOf0O3lpCTSM7JHDfnpXvWT0osfElRGghFS',
+  client_secret:
+    'EEakAyf3VsKxnEY7_nZIsqV16kE028kIBMewpq2Pq94R2bGWCs6syu725habPJMWa1OB552qP3u7dU9A',
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
+
+app.post('/pay', (req, res) => {
+  const create_payment_json = {
+    intent: 'sale',
+    payer: {
+      payment_method: 'paypal',
+    },
+    redirect_urls: {
+      return_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/cancel',
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: 'Fidget Spinner',
+              sku: '001',
+              price: '20.00',
+              currency: 'USD',
+              quantity: 1,
+            },
+          ],
+        },
+        amount: {
+          currency: 'USD',
+          total: '20.00',
+        },
+        description:
+          'A fidget spinner , perfect toy for distarcting yourself at school, home & work!',
+      },
+    ],
+  };
+
+  paypal.payment.create(create_payment_json, (error, payment) => {
+    if (error) {
+      throw error;
+    } else {
+      for (let i = 0; i < payment.links.length; i++) {
+        // prettier-ignore
+        if (payment.links[i].rel = 'approval_url') {
+          res.redirect(payment.links[i].href);
+        }
+      }
+    }
+  });
+});
+
+app.get('/success', (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    payer_id: payerId,
+    transactions: [
+      {
+        amount: {
+          currency: 'USD',
+          total: '20.00',
+        },
+      },
+    ],
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, (error, payment) => {
+    if (error) {
+      console.error(error.response);
+      throw error;
+    } else {
+      console.log(JSON.stringify(payment));
+      res.send('Successful Payment!');
+    }
+  });
+});
+
+app.get('/cancel', (req, res) =>
+  res.send('Payment Cancelled - An Error Has Occurred!')
+);
+
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
